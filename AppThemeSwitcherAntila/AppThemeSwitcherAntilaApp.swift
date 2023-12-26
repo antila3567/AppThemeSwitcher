4f9d1e982a7b15e58aaea12ff7eb707da5c446b8
IvanL <antila3567@gmail.com>
2023-12-26 13:51:19 +0200

    Initial Commit

---
+++ AppThemeSwitcherAntila/AppThemeSwitcherAntilaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppThemeSwitcherAntilaApp.swift
+//  AppThemeSwitcherAntila
+//
+//  Created by Иван Легенький on 26.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppThemeSwitcherAntilaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
